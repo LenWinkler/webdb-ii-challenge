2c5b193412cdadda5105cbf87c8e750c75289680
LenWinkler <lenwinkler86@gmail.com>
2019-11-05 21:30:40 -0500

    created database file and index and server files

--- data/migrations/20191105211522_add_cars_table.js
+++ data/migrations/20191105211522_add_cars_table.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('cars', function(table) {
+      table.increments();
+
+      table.string('vin', 17).notNullable();
+      table.string('make', 64).notNullable();
+      table.string('model', 64).notNullable();
+      table.integer('mileage').notNullable();
+      table.string('transmission', 16);
+      table.string('titleStatus', 64);
+
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
